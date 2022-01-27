5edc0e74d238dd08eec70ae0a1f3af2f2d32288d
Erin <erindb@stanford.edu>
2022-01-27 14:19:41 -0800

    start mimic corpus python file

---
+++ mimic_corpus.py
@@ -0,0 +1,9 @@
+# [x] get a corpus of example conversation
+
+# find the closest match in the corpus given the utterance
+
+#  bag of words similarity model (order of words in sentence doesn't matter)
+#  each word is a bunch of numbers (a vector)
+#  the whole sentence is going to be the average of those numbers
+
+# return the response to the closest match
