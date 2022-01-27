eb94996f806deee25f63d65b4c6e24239fce7916
Erin <erindb@stanford.edu>
2022-01-27 14:31:14 -0800

    start loading corpus

--- mimic_corpus.py
+++ mimic_corpus.py
@@ -1,5 +1,11 @@
+import pandas as pd
 # [x] get a corpus of example conversation
 
+# poe_corpus is a pandas dataframe
+poe_corpus = pd.read_csv("preprocessed_data.csv")
+# poe_stories is a pandas series
+poe_stories = poe_corpus["text"]
+print(poe_stories)
 # find the closest match in the corpus given the utterance
 
 #  bag of words similarity model (order of words in sentence doesn't matter)
