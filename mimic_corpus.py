9c6465aa7131bf587424f2d110d1f4a804db7bec
Kayla Lee <kayla@kaylas-air.lan>
2022-02-11 07:09:08 +0900

    we read in bible

--- mimic_corpus.py
+++ mimic_corpus.py
@@ -1,11 +1,20 @@
 import pandas as pd
 # [x] get a corpus of example conversation
 
-# poe_corpus is a pandas dataframe
-poe_corpus = pd.read_csv("preprocessed_data.csv")
-# poe_stories is a pandas series
-poe_stories = poe_corpus["text"]
-print(poe_stories)
+# # poe_corpus is a pandas dataframe
+# poe_corpus = pd.read_csv("poe_stories.csv")
+# # poe_stories is a pandas series
+# poe_stories = poe_corpus["text"]
+# print(poe_stories[0])
+
+# classical_corpus = pd.read_sql("classical_literature.db")
+
+bible_corpus = pd.read_csv("bible/t_wbt.csv")
+print(bible_corpus)
+bible_sentences = bible_corpus["t"]
+print(bible_sentences[0])
+
+
 # find the closest match in the corpus given the utterance
 
 #  bag of words similarity model (order of words in sentence doesn't matter)
@@ -13,3 +22,4 @@ print(poe_stories)
 #  the whole sentence is going to be the average of those numbers
 
 # return the response to the closest match
+
