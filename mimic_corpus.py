52b9be67048d6e095da08a66fbd2f169fb4e1909
Erin <erindb@stanford.edu>
2022-02-10 14:32:42 -0800

    add code to read in database

--- mimic_corpus.py
+++ mimic_corpus.py
@@ -1,13 +1,22 @@
 import pandas as pd
+import sqlite3
 # [x] get a corpus of example conversation
 
-# # poe_corpus is a pandas dataframe
-# poe_corpus = pd.read_csv("poe_stories.csv")
-# # poe_stories is a pandas series
-# poe_stories = poe_corpus["text"]
-# print(poe_stories[0])
+# poe_corpus is a pandas dataframe
+poe_corpus = pd.read_csv("poe_stories.csv")
+# poe_stories is a pandas series
+poe_stories = poe_corpus["text"]
 
-# classical_corpus = pd.read_sql("classical_literature.db")
+# conn = sqlite3.connect("classical_literature.db")
+# classical_corpus = pd.read_sql("select * from text_files", conn)
+# #classical_corpus_html = classical_corpus.loc[classical_corpus.fmt == "html"]
+# classical_corpus_txt = classical_corpus.loc[classical_corpus.fmt == "txt"]
+# classical_chapters = classical_corpus_txt["text"]
+# sentences = []
+# for chapter in classical_chapters:
+#   sentences += chapter.split("[.?!]")
+
+# print(sentences[:10])
 
 bible_corpus = pd.read_csv("bible/t_wbt.csv")
 print(bible_corpus)
