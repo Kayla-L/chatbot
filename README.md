3ed3c5ef25dcdda5bd1bb26003fad7201f886c19
Kayla Lee <kayla@kaylas-air.lan>
2022-03-04 06:43:45 +0900

    current state

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Classical corpora:
 - https://www.kaggle.com/leangab/poe-short-stories-corpuscsv
 - https://www.kaggle.com/nltkdata/shakespeare
 - https://www.kaggle.com/charlesaverill/gothic-literature
-- https://www.kaggle.com/raynardj/classic-english-literature-corpus?select=books.db
+- https://www.kaggle.com/raynardj/classic-english-literature-corpus 
 - https://www.kaggle.com/oswinrh/bible 
 
 TODO:
--- mimic_corpus.py
+++ mimic_corpus.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import texthero as hero
+from texthero import preprocessing
+
 # [x] get a corpus of example conversation
 
 # # poe_corpus is a pandas dataframe
@@ -10,10 +13,21 @@ import pandas as pd
 # classical_corpus = pd.read_sql("classical_literature.db")
 
 bible_corpus = pd.read_csv("bible/t_wbt.csv")
-print(bible_corpus)
 bible_sentences = bible_corpus["t"]
-print(bible_sentences[0])
 
+custom_pipeline = [preprocessing.fillna,
+                   #preprocessing.lowercase,
+                   preprocessing.remove_whitespace,
+                   preprocessing.remove_diacritics
+                   #preprocessing.remove_brackets
+                  ]
+bible_corpus['clean_text'] = hero.clean(bible_corpus['t'], custom_pipeline)
+print(bible_corpus)
+print(bible_sentences[0])
+# bible_corpus['clean_text'] = [n.replace('{','') for n in df['clean_text']]
+# bible_corpus['clean_text'] = [n.replace('}','') for n in df['clean_text']]
+# bible_corpus['clean_text'] = [n.replace('(','') for n in df['clean_text']]
+# bible_corpus['clean_text'] = [n.replace(')','') for n in df['clean_text']]
 
 # find the closest match in the corpus given the utterance
 
